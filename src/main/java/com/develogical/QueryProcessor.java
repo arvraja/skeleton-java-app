a9c38dd62571c059ee90f964506ead8fb2ae2bb2
arvraja <arv_r@yahoo.com>
2020-06-24 07:10:26 -0400

    Test change/build

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,6 +6,9 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
         }
+        if (query.toLowerCase().contains("gift of magi")) {
+            return "O'Henry";
+        }
         return "";
     }
 }
